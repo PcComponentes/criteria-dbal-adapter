dd97158bd278e9a2c9c44508a592d3b6ee493ddf
Ernesto <ernesto.gomez@pccomponentes.com>
2021-04-06 13:57:20 +0200

    fix code to use NullValueFilter

--- src/Criteria/DBALCriteriaVisitor.php
+++ src/Criteria/DBALCriteriaVisitor.php
@@ -5,8 +5,8 @@ namespace PcComponentes\CriteriaDBALAdapter\Criteria;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Pccomponentes\Criteria\Domain\Criteria\AndFilter;
 use Pccomponentes\Criteria\Domain\Criteria\Criteria;
-use Pccomponentes\Criteria\Domain\Criteria\Filter;
 use Pccomponentes\Criteria\Domain\Criteria\FilterInterface;
+use Pccomponentes\Criteria\Domain\Criteria\NullValueFilter;
 use Pccomponentes\Criteria\Domain\Criteria\FilterOperator;
 use Pccomponentes\Criteria\Domain\Criteria\FilterVisitorInterface;
 use Pccomponentes\Criteria\Domain\Criteria\OrFilter;
@@ -58,7 +58,7 @@ final class DBALCriteriaVisitor implements FilterVisitorInterface
         return '( '. $this->buildExpression($filter->left()) .' OR '. $this->buildExpression($filter->right()) .' )';
     }
 
-    public function visitFilter(Filter $filter): string
+    public function visitFilter(FilterInterface $filter): string
     {
         $this->countParams++;
 
@@ -67,6 +67,12 @@ final class DBALCriteriaVisitor implements FilterVisitorInterface
             $this->mapParameter($filter),
             $this->mapType($filter),
         );
+
+        if ($filter instanceof NullValueFilter) {
+            return $this->mapFieldValue($filter->field()->value())
+                . ' '
+                . $filter->operator()->value();
+        }
 
         return $this->mapFieldValue($filter->field()->value())
             . ' '
@@ -82,7 +88,7 @@ final class DBALCriteriaVisitor implements FilterVisitorInterface
         return $filter->accept($this);
     }
 
-    private function mapType(Filter $filter): ?int
+    private function mapType(FilterInterface $filter): ?int
     {
         if (\in_array($filter->operator()->value(), [FilterOperator::IN, FilterOperator::NOT_IN])) {
             return \Doctrine\DBAL\Connection::PARAM_STR_ARRAY;
@@ -91,7 +97,7 @@ final class DBALCriteriaVisitor implements FilterVisitorInterface
         return null;
     }
 
-    private function mapOperator(Filter $filter): string
+    private function mapOperator(FilterInterface $filter): string
     {
         if (FilterOperator::CONTAINS === $filter->operator()->value()) {
             return 'LIKE';
@@ -104,8 +110,12 @@ final class DBALCriteriaVisitor implements FilterVisitorInterface
         return $filter->operator()->value();
     }
 
-    private function mapParameter(Filter $filter)
+    private function mapParameter(FilterInterface $filter)
     {
+        if (\in_array($filter->operator()->value(), [FilterOperator::IS_NULL, FilterOperator::IS_NOT_NULL])) {
+            return;
+        }
+
         if (FilterOperator::CONTAINS === $filter->operator()->value()) {
             return '%' . $filter->value()->value() . '%';
         }
